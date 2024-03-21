5125b6622efeb113cf60c1186eae93c9643a387c
ehsankeyhan <ehsankeyhan@users.noreply.github.com>
2024-03-21 21:21:18 +0000

    deploy: e40ba6b39110b13ef6ccd9da68532984a8bd68fd

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base:'/roocket-1/',
-  plugins: [react()],
-})
